e6b646916817edda32c181a278ef7ee38a014287
Arnold Patten <apatten001@yahoo.com>
2022-03-03 13:06:00 -0500

    update info forexposing the ids for details url path

--- src/main/java/com/hcl/config/MyDataRestConfig.java
+++ src/main/java/com/hcl/config/MyDataRestConfig.java
@@ -56,7 +56,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 	
 	private void exposeIds(RepositoryRestConfiguration config) {
 		
-		// expose entity ids
+		// expose entity ids for the access of the details path
 		
 		// get a list of all entity classes from the entity manager
 		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
