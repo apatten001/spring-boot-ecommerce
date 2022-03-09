12cb8f407e4e336f7770f0967117886817448c1d
Arnold Patten <apatten001@yahoo.com>
2022-03-09 12:19:05 -0500

    Implement a checkout service for placing an order to save to the database

--- src/main/java/com/hcl/entity/OrderItem.java
+++ src/main/java/com/hcl/entity/OrderItem.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -37,5 +39,7 @@ public class OrderItem {
 	@Column(name="order_id")
 	private Long orderId;
 	
+	@ManyToOne
+	@JoinColumn(name="order_id")
 	private Order order;
 }
---
+++ src/main/java/com/hcl/service/CheckoutServiceImpl.java
@@ -0,0 +1,82 @@
+package com.hcl.service;
+
+
+import java.util.Set;
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hcl.dao.CustomerRepository;
+import com.hcl.dto.Purchase;
+import com.hcl.dto.PurchaseResponse;
+import com.hcl.entity.Customer;
+import com.hcl.entity.Order;
+import com.hcl.entity.OrderItem;
+
+@Service
+public class CheckoutServiceImpl implements CheckoutService{
+	
+	private CustomerRepository customerRepository;
+	
+	
+	// inject customer Repo into the Customer service
+	public CheckoutServiceImpl(CustomerRepository customerRepository) {
+		
+		this.customerRepository = customerRepository;
+		
+	}
+
+	@Override
+	@Transactional
+	public PurchaseResponse placeOrder(Purchase purchase) {
+		
+		// retrieve the order info from the dto
+		
+		Order order  = purchase.getOrder();
+		
+		// generate tracking number
+		String orderTrackingNumber = generateOrderTrackingNumber();
+		order.setOrderTrackingNumber(orderTrackingNumber);
+		
+		// populate orders with orderItem's
+		Set<OrderItem> orderItems = purchase.getOrderItems();
+		orderItems.forEach(item -> order.add(item));
+		
+		
+		
+		//populate order with billing Address and shippingAddress
+		// populate order with billingAddress
+		order.setBillingAddress(purchase.getBillingAddress());
+		order.setShippingAddress(purchase.getShippingAddress());
+		
+		
+		// populate customer with order
+		
+		Customer customer = purchase.getCustomer();
+		customer.add(order);
+		
+		// save to the database 
+		
+		customerRepository.save(customer);
+		
+		// return a response
+		return new PurchaseResponse(orderTrackingNumber);
+		
+		
+		
+		
+		
+	}
+
+	private String generateOrderTrackingNumber() {
+		
+		// generate random UUID number 
+		// (Unique Universal IDentifier) for generating unique Id's
+		
+		return UUID.randomUUID().toString();
+	}
+
+}
