88fef8930083074557a6822921ef7797691111d3
Arnold Patten <apatten001@yahoo.com>
2022-03-10 15:37:03 -0500

    add findByEmail method to search by email

--- src/main/java/com/hcl/dao/CustomerRepository.java
+++ src/main/java/com/hcl/dao/CustomerRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.hcl.entity.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
+	
+	// returns customer if their null if email not there
+	Customer findByEmail(String theEmail);
 
 }
